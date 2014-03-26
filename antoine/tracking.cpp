1c3c696f8f1a61e698951bde4bf7b183af803c37
AnKaRobot <antoine.kaszczyc@gmail.com>
2014-03-26 16:45:03 +0100

    Trackbar dans la vidéo pour le flou et erode

--- antoine/tracking.cpp
+++ antoine/tracking.cpp
@@ -5,16 +5,47 @@ using namespace cv;
 
 int main(int argc, char *argv[]) {
 
+	char c;
+
 	VideoCapture cap(0);
 	if (!cap.isOpened()) {
 		return -1;
 	}
-	namedWindow("edges", 1);
+	namedWindow("base", 1);
+	Mat frame;
+	
+	int erodeVal = 0,
+		smoothVal = 0;
+
+	createTrackbar("erode", "base", &erodeVal, 30);
+	createTrackbar("smooth", "base", &smoothVal, 100);
+
+	Mat element;
+
 	while (1) {
-		Mat frame;
 		cap >> frame;
-		imshow("edges", frame);
-		if(waitKey(30) >= 0) { break; }
+		if (frame.empty() ) {
+			printf("***problème frame***\n");
+		}
+		else {
+			if (erodeVal) {
+				element = getStructuringElement(MORPH_ELLIPSE, Size(erodeVal, erodeVal));
+				erode(frame, frame, element);
+			}
+			if (smoothVal) {
+				blur(frame, frame, Size(smoothVal, smoothVal));
+			}
+			//cvtColor(frame, frame, COLOR_RGB2HSV);
+			imshow("base", frame);
+		}
+		c = (char) cvWaitKey(30);
+		if (c == 27) {
+			break;
+		}
 	}
 	return 0;
 }
+/*
+void barreAction(int, void*) {
+	int barreValue = getTrackbarPos("barre", "base");
+}*/
