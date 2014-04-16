0748710df9671539aaa1579dfbf8b12ee56acb43
zx900930 <zx900930@gmail.com>
2014-04-16 22:16:55 +0200

    normalization BGR

---
+++ xiong/normalization/normalization.cpp
@@ -0,0 +1,44 @@
+#include "opencv2/highgui/highgui.hpp"
+#include "opencv2/imgproc/imgproc.hpp"
+#include <iostream>
+
+using namespace cv;
+using namespace std;
+
+int main( int argc, const char** argv )
+{
+       Mat img = imread("MyPic.JPG", CV_LOAD_IMAGE_COLOR); 
+
+       if (img.empty()) 
+       {
+            cout << "Image cannot be loaded..!!" << endl;
+            return -1;
+       }
+
+       vector<Mat> channels; 
+       Mat img_hist_equalized;
+
+       cvtColor(img, img_hist_equalized, CV_BGR2YCrCb); 
+
+       split(img_hist_equalized,channels); 
+
+       equalizeHist(channels[0], channels[0]);
+
+   merge(channels,img_hist_equalized); 
+
+      cvtColor(img_hist_equalized, img_hist_equalized, CV_YCrCb2BGR); 
+
+       
+       namedWindow("Original Image", CV_WINDOW_NORMAL);
+       namedWindow("Histogram Equalized", CV_WINDOW_NORMAL);
+
+      
+       imshow("Original Image", img);
+       imshow("Histogram Equalized", img_hist_equalized);
+
+       waitKey(0); 
+
+       destroyAllWindows(); 
+
+       return 0;
+}
